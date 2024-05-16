73fb121461b04a24b9c930cbfb7b528b068d5234
NajwaAfifi <114920676+NajwaAfifi@users.noreply.github.com>
2024-05-16 17:02:01 +0700

    Update AnagramString.java

--- AnagramString.java
+++ AnagramString.java
@@ -1,15 +1,1 @@
 
-
- 
-        // Membandingkan panjang kedua string
-      
-        // Mengonversi string menjadi array karakter dan mengurutkannya
-        char[] charArray1 = str1.toCharArray();
-        char[] charArray2 = str2.toCharArray();
-        Arrays.sort(charArray1);
-        Arrays.sort(charArray2);
-
-        // Membandingkan array karakter yang sudah diurutkan
-        return Arrays.equals(charArray1, charArray2);
-    }
-}
